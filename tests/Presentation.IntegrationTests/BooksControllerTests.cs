da13449e227f324d1b3fcd38b956d8cfd6e4be81
Diego Pereira <diego.pereira@farfetch.com>
2023-12-19 22:09:55 +0000

    chore: github workflow

--- tests/Presentation.IntegrationTests/BooksControllerTests.cs
+++ tests/Presentation.IntegrationTests/BooksControllerTests.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 
 namespace Presentation.IntegrationTests
 {
+    [Trait("Category", "Integration Tests")]
     public class BooksControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _factory;
